bcf170bfccc82b8661b1b36aa3a2c0c3babc3461
Patorn Kwanthale <the_dann@hotmail.co.th>
2023-04-30 02:29:41 +0700

    INIT COMMIT

---
+++ atifacts-calc/src/test/java/professor/why/atifactscalc/AtifactsCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package professor.why.atifactscalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtifactsCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
